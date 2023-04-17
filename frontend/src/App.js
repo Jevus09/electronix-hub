537775ddd8502bedb0f6c1dfce31e0fb47c4bca6
jevus09 <jevus09@gmail.com>
2023-04-17 14:16:32 -0500

    set structure added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import {Container} from 'react-bootstrap'
 
 
 
@@ -6,7 +8,13 @@
 const App = () => {
   return (
     <>
-     <h1>Welcome to my site</h1> 
+    <Header/>
+    <main className='py-3' >
+      <Container>
+    <h1>Welcome to my site</h1> 
+    </Container>
+    </main>
+    <Footer/>
     </>
   );
 }
