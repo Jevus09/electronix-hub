c6dc04b019ee0312aa829137c3f7557eceb15b97
jevus09 <jevus09@gmail.com>
2023-04-17 17:10:33 -0500

    added router updated a tags to <Links/>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import {Container} from 'react-bootstrap'
-import HomeScreen from './screens/HomeScreen';
-
-
-
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
-    <Header/>
-    <main className='py-3' >
-      <Container>
-     <HomeScreen/>
-    </Container>
-    </main>
-    <Footer/>
-    </>
-  );
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' Component={HomeScreen} exact />
+            <Route path='/product/:id' Component={ProductScreen} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
