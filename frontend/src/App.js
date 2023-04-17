2117b9ed57692cdd9901eab490b46dae37f45509
jevus09 <jevus09@gmail.com>
2023-04-17 14:42:05 -0500

    added temp images, created product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import {Container} from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen';
 
 
 
@@ -11,7 +12,7 @@ const App = () => {
     <Header/>
     <main className='py-3' >
       <Container>
-    <h1>Welcome to my site</h1> 
+     <HomeScreen/>
     </Container>
     </main>
     <Footer/>
