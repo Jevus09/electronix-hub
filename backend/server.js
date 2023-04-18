10bd420f6156453270d9b051f9546fce48749786
jevus09 <jevus09@gmail.com>
2023-04-18 18:48:20 -0500

    added custom erroir handleling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import dotenv from'dotenv'
 import connectdb from './config/db.js'
-import products from './data/products.js'
 import colors from 'colors'
+import prductRoutes from './routes/productRoutes.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 const app = express()
 connectdb()
@@ -12,13 +13,12 @@ dotenv.config()
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+
+app.use('/api/products', prductRoutes)
+
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
