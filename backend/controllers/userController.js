43b62490b855954e7d1b5362c82c38af362cd9ee
jevus09 <jevus09@gmail.com>
2023-04-24 17:22:09 -0500

    initiated token setup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,29 @@
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+import generateToken from './utils/generateToke.js'
+
+// @desc Auth user and get token
+// @route POST /api/user/login
+// @access Public
+const authUser = asyncHandler( async (req, res) => {
+    const { email, password } = req.body
+
+
+    const user= await User.findOne({email})
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid id or password')
+    }
+
+})
+
+export {authUser}
--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,32 @@ import express from 'express'
 import dotenv from'dotenv'
 import connectdb from './config/db.js'
 import colors from 'colors'
-import prductRoutes from './routes/productRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/usersRoutes.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
-const app = express()
+
 connectdb()
 
 dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.use('/api/products', prductRoutes)
-
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
+
+
+
 
 const PORT = process.env.PORT || 5000
 
