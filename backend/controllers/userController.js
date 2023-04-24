03421c59dd0a9fb998d9a8683995664b16efbf80
jevus09 <jevus09@gmail.com>
2023-04-24 18:30:13 -0500

    completed backend authentication/token setup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
-import generateToken from './utils/generateToke.js'
+import generateToken from '../utils/generateToke.js'
 
 // @desc Auth user and get token
 // @route POST /api/user/login
@@ -26,4 +26,65 @@ const authUser = asyncHandler( async (req, res) => {
 
 })
 
-export {authUser}
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler( async (req, res) => {
+    const { name, email, password } = req.body
+
+
+    const userExists = await User.findOne({email})
+
+    if (userExists ) {
+        res.status(400)
+        throw new Error('Users already exists')
+    } 
+
+    const user = await User.create({
+        name,
+        email, 
+        password
+    })
+
+    if (user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error("invalid user data")
+    }
+
+
+})
+
+
+
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler( async (req, res) => {
+    const user= await User.findById(req.user._id)
+
+    if (user ) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+
+
+})
+
+export {authUser, getUserProfile, registerUser}
--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 
+
 app.use(notFound)
 app.use(errorHandler)
 
