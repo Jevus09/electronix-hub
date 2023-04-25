6d8672f574fa8dc7eea436dfc9049f9673d5cef7
jevus09 <jevus09@gmail.com>
2023-04-25 17:04:40 -0500

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 
 const App = () => {
   return (
@@ -13,10 +14,11 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Routes>
-            <Route path='/' element={<HomeScreen/>} exact />
+          <Routes>            
+            <Route path='/login' element={<LoginScreen/>} />
             <Route path='/product/:id' element={<ProductScreen/>} />
             <Route path='/cart/:id?' element={<CartScreen/>} />
+            <Route path='/' element={<HomeScreen/>} exact />
           </Routes>
         </Container>
       </main>
---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,81 @@
+import {React, useState, useEffect, useDebugValue} from 'react'
+import {Link, useNavigate } from 'react-router-dom'
+import FormContainer from '../components/FormContainer'
+import { Button, Col, Form, Row } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../actions/userActions'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+
+
+
+
+
+
+const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector(state => state.userLogin)
+    const {loading, error, userInfo} = userLogin
+ 
+    const redirect = navigate.search ? navigate.search.split('=')[1] : '/'
+
+    useEffect(() => {
+        if(userInfo){
+            navigate(redirect)
+        }
+    }, [navigate, userInfo, redirect])
+
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(login(email, password))
+    }
+
+
+
+
+
+
+  return (
+    <FormContainer>
+        <h1>Sign In</h1>
+        {error && <Message variant='danger'>{error}</Message>}
+        {loading && <Loader/>}
+        <Form onSubmit={submitHandler}>
+            <Form.Group controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type='emial' placeholder='Enter email' value={email}
+            onChange={(e) => setEmail(e.target.value)}>
+            </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type='password' placeholder='Enter password' value={password}
+            onChange={(e) => setPassword(e.target.value)}>                
+            </Form.Control>
+            </Form.Group>
+
+            <Button type='submit' variant='primary'>
+                Sign In
+            </Button>
+        </Form>
+        <Row className='py-3' >
+            <Col>
+            New Customer?{' '}
+            <Link to={redirect ? `/register?redirect=${redirect}` : '/redirect'}> Register</Link> 
+            </Col>
+
+        </Row>
+
+
+        </FormContainer>
+  )
+}
+
+export default LoginScreen
