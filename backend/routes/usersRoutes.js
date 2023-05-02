dc5fa8f20237e9564d5a4ad7efc2828ba7da0b06
jevus09 <jevus09@gmail.com>
2023-05-02 14:55:24 -0500

    initiated set up for admin login features

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,14 +1,16 @@
 import express from "express";
-import { authUser, getUserProfile, registerUser, updateUserProfile } from "../controllers/userController.js"
-import { protect } from "../middleware/authMiddleware.js"
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } from "../controllers/userController.js"
+import { protect, admin } from "../middleware/authMiddleware.js"
  
 
 const router = express.Router()
 
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser) 
+router.route('/').get(protect, admin, getUsers) 
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+
 
 
 
